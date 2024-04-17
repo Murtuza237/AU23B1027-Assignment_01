e3fe094f65e4d5080418986e2ed7fc367cafdb7e
Murtuza Ali <148961257+Murtuza237@users.noreply.github.com>
2024-04-17 21:37:32 +0530

    Create ice_cream.py

---
+++ ice_cream.py
@@ -0,0 +1,10 @@
+def add_topping(Scoop_Size,top):
+    print("choose scoop size from->",Scoop_Size)
+    x=input()
+    print("choose toppings->",top)
+    y=input()
+    return x,y
+def make_shake(options):
+    print("choose options for shake->",options)
+    z=input("")
+    return z
