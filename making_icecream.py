a827e888d2c053ad633888f7df20ad1040df43c9
Murtuza Ali <148961257+Murtuza237@users.noreply.github.com>
2024-04-17 21:39:06 +0530

    Create making_icecream.py

---
+++ making_icecream.py
@@ -0,0 +1,7 @@
+from ice_cream import*
+Scoop_Size=["small","medium","big"]
+top=["sprinkles","hot fudge","whipped cream","crushed nuts"]
+options=["nuts","fruits"] 
+add_topping(Scoop_Size,top)
+make_shake(options)
+print("you ice cream is ready to go!!!")
