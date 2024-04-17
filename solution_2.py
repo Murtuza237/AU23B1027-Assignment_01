e701ba0f32aa9f2358d271eb84b16f478c595dff
Murtuza Ali <148961257+Murtuza237@users.noreply.github.com>
2024-04-17 22:37:29 +0530

    Update solution_2.py

--- solution_2.py
+++ solution_2.py
@@ -3,16 +3,18 @@ def get_name():
     return x 
 def get_tshirt():
     x=get_name()
-    y=input("What brand are you looking for:")
+    y=input("which brand do you want:")
     size=input("Which size do you want:")
     a=["puma","nike","peter england","louis viton"]
     b=["l","xl","xxl"]
     for i in a:
         for ele in b:
-            if i==y and ele==size:
+            if i!=y or ele!=size:
+                continue
+            elif i==y and ele==size:
                 print("Hii"+" "+x+","+"the brand you are looking for with size "+size+" is available in our store ")
-                break    
-        else:
-            print("Hii"+" "+x+","+"Unfortunately the brand you are looking for with size "+size+" is not available in our store ") 
-        break     
+                break  
+            else:
+                print("Hii"+" "+x+","+"Unfortunately the brand you are looking for with size "+size+" is not available in our store ") 
+                break
 get_tshirt()
